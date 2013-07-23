af67c61299d7e6e2a62397edaef9d55c44df843f
xeostream <iostreamin@gmail.com>
2013-07-23 21:53:41 +0800

    edit zoj1241, non-zero exit code

--- zoj1241.py
+++ zoj1241.py
@@ -1,24 +1,25 @@
-from math import pos, sqrt
+from math import pow, sqrt
 case = raw_input()
 result = []
 while case != '0 0 0':
     temp = case.split()
     if temp[0] == '-1':
-        aa = pos(temp[2], 2) - pos(temp[1], 2)
+        aa = pow(float(temp[2]), 2) - pow(float(temp[1]), 2)
         if aa > 0:
             a = sqrt(aa)
             result.append({'a': a})
         else:
-            result.append('Impossible')
+            result.append('Impowsible')
     elif temp[1] == '-1':
-        bb = pos(temp[2], 2) - pos(temp[0], 2)
+        bb = pow(float(temp[2]), 2) - pow(float(temp[0]), 2)
         if bb > 0:
             b = sqrt(bb)
             result.append({'b': b})
         else:
-            result.append('Impossible')
+            result.append('Impowsible')
     elif temp[2] == '-1':
-        c = sqrt(pos(temp[0], 2) + sqrt(temp[1], 2))
+        tem = pow(float(temp[0]), 2) + pow(float(temp[1]), 2)
+        c = sqrt(tem)
         result.append({'c': c})
     case = raw_input()
 
@@ -26,12 +27,9 @@ length = len(result)
 for x in range(length):
     print 'Triangle #%i' %(x + 1)
     if result[x] == 'Impossible':
-        print 'Impossible'
+        print 'Impowsible'
     else:
         temp = result[x]
         for y in temp:
             print '%s = %.3f' %(y, temp[y])
     print
-    
-        
-        
